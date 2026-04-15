929137452d88650685e99c43c99f5bd8dbbbda09
Fiona-091305 <349460105@gapps.yrdsb.ca>
2026-04-15 10:39:53 -0400

    add hero

---
+++ hero.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+
+public class hero extends Actor
+{
+    boolean atTop = true;
+    public void act()
+    {
+        if(Greenfoot.mouseClicked(null))
+        {
+            setLocation(100, 100);
+            atTop = !atTop;
+        }
+        
+        if(atTop)
+        {
+            setLocation(100,100);
+        }
+        else
+        {
+            setLocation(100, 300);
+        }
+    }
+}
